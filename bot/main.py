b3f03c1696397c57af6b31eb9b991c8c69fdb2f6
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2019-10-06 23:11:25 +0300

    Добавлена улучшеная обратная связь с админами
    Добавлена улучшеная обратная связь с администраторами сервиса.

--- bot/main.py
+++ bot/main.py
@@ -25,12 +25,20 @@ while True:
                         for arr1 in raspisanie[arr].items():
                             persent = ""
                             response_2.append(persent)
+                            #print(response_2)
                             if str(response_2[1]) == str(arr1[0]):
                                 vk_session.method('messages.send', {'user_id': event.user_id, 'message': str(arr1[1]), 'random_id': 0})
                             elif str(response_2[1]) == "":
                                 mess = "Введите корректную команду. \nЧтобы узнать список команды напишите мне Помощь."
                                 vk_session.method('messages.send', {'user_id': event.user_id, 'message': mess, 'random_id': 0})
                                 break
+                # === Отправка сообщения администратору ===
+                if str(response_2[0]) == "админу":
+                    mess = "Ваше сообщение отправлено."
+                    mess_admin = "Вам написал пользователь @id" + str(event.user_id) + " " +" ".join(str(x) for x in response_2[1:])
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': mess, 'random_id': 0})
+                    vk_session.method('messages.send', {'user_id': 202477769, 'message': mess_admin, 'random_id': 0})
+                # === Инстукция по использованию бота ===
                 if str(response_2[0]) == "помощь":
-                    mess = "Расписание, [номер группы] [день недели]" + "\n" + "Примеры команд: \nзрс1701 вт \nбрв1701 пт \nбст1901 ср" + "\n\n" + "Обратная связь" + "\n" + "Чтобы обратиться к администраторам сервиса с предложением для улучшения сервиса, коррекцией расписания или другим вопросам напишите нам на почту yetanothercompany2019@gmail.com"
+                    mess = "Расписание, [номер группы] [день недели]" + "\n" + "Примеры команд: \nзрс1701 вт \nбрв1701 пт \nбст1901 ср" + "\n\n" + "Обратная связь" + "\n" + "Чтобы обратиться к администраторам сервиса с предложением для улучшения сервиса, коррекцией расписания или другим вопросам отправьте команду \nадмину [ваше сообщение] \n или напишите нам на почту yetanothercompany2019@gmail.com"
                     vk_session.method('messages.send', {'user_id': event.user_id, 'message': mess, 'random_id': 0})
